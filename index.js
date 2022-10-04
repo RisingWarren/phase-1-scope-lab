17fc48b5e9593915f79bb503f1ea93fd3e5f8da3
Warren Santiago <wsantiago9623@gmail.com>
2022-10-03 20:16:23 -0400

    commit tree 8e752c9633304a2660cd35d3f0105dbe9357541d

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+
+
+var customerName = "bob"
+
+const leastFavoriteCustomer = "definitely bob"
+
+
+function upperCaseCustomerName(){
+
+    return customerName =  `${customerName}`.toUpperCase();
+}
+
+function setBestCustomer(){
+
+    bestCustomer = "not bob";
+
+    return bestCustomer;
+
+
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = "greg"
+
+}
+
+changeLeastFavoriteCustomer()
